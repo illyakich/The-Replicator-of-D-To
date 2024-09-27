fd5f5ef0e3781d042aeed224c4e052d8829db2d7
illyakich <speedfeed123@gmail.com>
2024-09-27 22:22:35 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿int[] array = new int[5] {Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())};
+/* for  (int i = 0; i < 5; i++)
+    Console.WriteLine(array[i]); */
+int[] jarray = new int[5];
+
+// copies the values of one array into the other
+for (int i = 0; i < 5; i++)
+    jarray[i] = array[i];
+
+// added for readibility
+Console.WriteLine("---------------");
+
+// prints the values of the array "array" (C# just wont let your Console.WriteLine(array); to show it's contents.
+for (int i = 0; i < 5; i++)
+    Console.WriteLine(array[i]);
+
+//added for readibility
+Console.WriteLine("---------------");
+
+// prints the values of the array "jarray".
+for (int i = 0; i < 5; i++)
+    Console.WriteLine(jarray[i]);
+// lets gooo
